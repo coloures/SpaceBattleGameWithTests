cd48649e9307a8c176d17faaeb61bdfb2b527815
Litau-Vladislav <litau05@mail.ru>
2024-12-06 21:52:52 +0600

    IMoving and IRotating Objects

---
+++ SpaceBattle.Tests/AngleTest.cs
@@ -0,0 +1,81 @@
+namespace SpaceBattle.Tests;
+using SpaceBattle.Lib;
+using Moq;
+
+public class AngleTest
+{
+    [Fact]
+    public void TestSameAnglesAreEqual()
+    {
+        var angl1 = new Angle(12);
+        var angl2 = new Angle(12);
+        Assert.NotSame(angl1, angl2);
+        Assert.Equal(angl1, angl2);
+    }
+
+    [Fact]
+    public void TestAdditionThrowsException()
+    {
+        Angle angl1 = null;
+        Angle angl2 = new Angle(12);
+        Assert.Throws<ArgumentNullException>(() => angl1 + angl2);
+    }
+
+    [Fact]
+    public void TestComparingAngleWithNotAngleType()
+    {
+        Angle angl1 = new Angle(12);
+        bool Equality = angl1.Equals(12);
+        Assert.False(Equality);
+    }
+
+    [Fact]
+    public void TestEqualsObjectNull()
+    {
+        Angle angl1 = new Angle(12);
+        bool Equality = angl1.Equals(null);
+        Assert.False(Equality);
+    }
+
+    [Fact]
+    public void TestSameAnglesAreNotEqual()
+    {
+        var angl1 = new Angle(10);
+        var angl2 = new Angle(12);
+        Assert.NotEqual(angl1, angl2);
+    }
+
+    [Fact]
+    public void TestSummationPositive()
+    {
+        var angl1 = new Angle(10);
+        var angl2 = new Angle(12);
+        var angl3 = angl1 + angl2;
+        Assert.Equal(angl3, new Angle(22));
+    }
+
+    [Fact]
+    public void TestSummationNegative()
+    {
+        var angl1 = new Angle(10);
+        var angl2 = new Angle(-30);
+        var angl3 = angl1 + angl2;
+        Assert.Equal(angl3, new Angle(-20));
+    }
+    [Fact]
+    public void TestGetHashCode()
+    {
+        var angle1 = new Angle(54);
+        var angle2 = new Angle(54);
+        Assert.True(angle1.GetHashCode() == angle2.GetHashCode());
+    }
+
+    [Fact]
+    public void TestNewImplication()
+    {
+        var angle1 = new Angle(12);
+        angle1.A = 13;
+
+        Assert.Equal(13, angle1.A);
+    }
+}
