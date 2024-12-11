5e446bd17831fa5e346b25f6e4ab3a001150319b
coloures <glebbindjuk@gmail.com>
2024-12-11 20:38:22 +0600

    1 commit

---
+++ SpaceBattle.Lib/RegisterDependencyCommandInjectableCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace SpaceBattle.Lib;
+using App;
+public class RegisterDependencyCommandInjectableCommand : ICommand
+{
+    public void Execute()
+    {
+        var RegisterDependencyCommandInjectableCommand = Ioc.Resolve<App.ICommand>(
+          "IoC.Register",
+          "Commands.CommandInjectable",
+          (object[] args) => new BridgeCommand()
+          );
+        RegisterDependencyCommandInjectableCommand.Execute();
+    }
+}
---
+++ SpaceBattle.Tests/RegisterDependencyCommandInjectableCommandTest.cs
@@ -0,0 +1,12 @@
+﻿using App;
+using App.Scopes;
+namespace SpaceBattle.Tests;
+public class RegisterDependencyCommandInjectableCommandTest
+{
+    public RegisterDependencyCommandInjectableCommandTest()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+        Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+}
