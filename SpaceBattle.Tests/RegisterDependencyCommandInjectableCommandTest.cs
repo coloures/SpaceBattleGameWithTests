0f9098b45f6096d056b9547b1b98eaa441c47229
coloures <glebbindjuk@gmail.com>
2024-12-11 20:41:15 +0600

    Tests are fixed

--- SpaceBattle.Tests/RegisterDependencyCommandInjectableCommandTest.cs
+++ SpaceBattle.Tests/RegisterDependencyCommandInjectableCommandTest.cs
@@ -9,4 +9,17 @@ public class RegisterDependencyCommandInjectableCommandTest
         var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
         Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
     }
+    [Fact]
+    public void CommandInjectableIsRegistered()
+    {
+        var temp = new SpaceBattle.Lib.RegisterDependencyCommandInjectableCommand();
+        temp.Execute();
+        var _BridgeCommand1 = Ioc.Resolve<SpaceBattle.Lib.ICommand>("Commands.CommandInjectable");
+        var _BridgeCommand2 = Ioc.Resolve<SpaceBattle.Lib.IInjectable>("Commands.CommandInjectable");
+        var _BridgeCommand3 = Ioc.Resolve<SpaceBattle.Lib.BridgeCommand>("Commands.CommandInjectable");
+        Assert.IsType<SpaceBattle.Lib.BridgeCommand>(_BridgeCommand1);
+        Assert.IsType<SpaceBattle.Lib.BridgeCommand>(_BridgeCommand2);
+        Assert.IsType<SpaceBattle.Lib.BridgeCommand>(_BridgeCommand3);
+
+    }
 }
