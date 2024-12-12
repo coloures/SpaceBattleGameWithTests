4a04e2514b3659de21773f850a8041f7cc8be609
KronshtadtovMaxim <maks.kronshtadtov@mail.ru>
2024-12-12 17:21:22 +0600

    lab2

--- SpaceBattle.Tests/StopMoveCommandTest.cs
+++ SpaceBattle.Tests/StopMoveCommandTest.cs
@@ -11,10 +11,8 @@ public class StopMoveCommandTest
         var _gameobject = new Mock<IDictionary<string, object>>();
         _gameobject.SetupGet(m => m["Movement"]).Returns(new Mock<ICommand>()).Verifiable();
         var StopMoveCommand = new StopMoveCommand(_isender.Object, _gameobject.Object);
-
         //act
         StopMoveCommand.Execute();
-
         //post
         _gameobject.VerifySet(m => m["Movement"] = It.IsAny<EmptyCommand>(), Times.Once);
         _isender.Verify(sender => sender.Send(It.IsAny<EmptyCommand>()), Times.Once);
@@ -27,11 +25,9 @@ public class StopMoveCommandTest
         var _gameobject = new Mock<IDictionary<string, object>>();
         _gameobject.SetupSet(m => m["Movement"] = It.IsAny<ICommand>()).Throws<Exception>();
         var StopMoveCommand = new StopMoveCommand(_isender.Object, _gameobject.Object);
-
         Assert.Throws<Exception>( //Assert
             () => StopMoveCommand.Execute()//Act
         );
-
         //post
         _isender.Verify(sender => sender.Send(It.IsAny<ICommand>()), Times.Never);
     }
@@ -43,11 +39,9 @@ public class StopMoveCommandTest
         var _gameobject = new Mock<IDictionary<string, object>>();
         _isender.Setup(m => m.Send(It.IsAny<ICommand>())).Throws<Exception>();
         var StopMoveCommand = new StopMoveCommand(_isender.Object, _gameobject.Object);
-
         Assert.Throws<Exception>( //Assert
             () => StopMoveCommand.Execute()//Act
         );
-
         //post
         _gameobject.VerifySet(m => m["Movement"] = It.IsAny<EmptyCommand>(), Times.Once);
         _isender.Verify(sender => sender.Send(It.IsAny<EmptyCommand>()), Times.Once);
