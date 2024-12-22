ab51cbff0aee5b1111d0cb744947338f007ade61
Litau-Vladislav <litau05@mail.ru>
2024-12-22 17:54:01 +0600

    angleextensions

---
+++ SpaceBattle.Lib/AngleExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace SpaceBattle.Lib
+{
+    public static class AngleExtensions
+    {
+        public static double ToDouble(this Angle angle)
+        {
+            if (angle == null)
+            {
+                throw new ArgumentNullException(nameof(angle), "Угол не может быть null");
+            }
+
+            return (double)angle.A;
+        }
+    }
+}
