a6ecc455170e2b76edaccddbfd208c8d014d1d42
Заславский Ярослав Максимович <blackfyz00@gmail.com>
2024-12-24 23:03:31 +0600

    RegisterIoC Macro.Move Command

---
+++ SpaceBattle.Tests/RegisterIoCDependencyMacroMoveTests.cs
@@ -0,0 +1,67 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+namespace SpaceBattle.Tests;
+public class RegisterIoCDependencyMacroMoveTests : IDisposable
+{
+    public RegisterIoCDependencyMacroMoveTests()
+    {
+        new InitCommand().Execute();
+        var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+
+        Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+    }
+    [Fact]
+    public void MacroMoveCommandIsRegistered()
+    {
+        var cmd = new Mock<Lib.ICommand>();
+        cmd.Setup(x => x.Execute()).Verifiable();
+        var temp = new Lib.RegisterIoCDependencyMacroMove();
+        temp.Execute();
+        Ioc.Resolve<App.ICommand>(
+          "IoC.Register",
+          "Specs.Move",
+          (object[] args) =>
+            new string[] { "Any.Move" }).Execute();
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Any.Move",
+            (object[] args) =>
+            cmd.Object).Execute();
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Command.Macro",
+            (object[] args) =>
+            {
+                var cmds = (SpaceBattle.Lib.ICommand[])args;
+                return new SpaceBattle.Lib.MacroCommand(cmds);
+            }
+            ).Execute();
+        var AnyObj = new Mock<object>();
+
+        var MoveCmd = Ioc.Resolve<Lib.MacroCommand>("Macro.Move", AnyObj.Object);
+
+        MoveCmd.Execute();
+
+        cmd.Verify(x => x.Execute(), Times.Once);
+
+    }
+    [Fact]
+    public void MacroMoveCommandIsNotRegistered()
+    {
+        var cmd = new Mock<Lib.ICommand>();
+        cmd.Setup(x => x.Execute()).Verifiable();
+        var AnyObj = new Mock<object>();
+
+        Assert.Throws<Exception>(() =>
+        {
+            var MoveCmd = Ioc.Resolve<Lib.MacroCommand>("Macro.Move", AnyObj.Object);
+            MoveCmd.Execute();
+        });
+        cmd.Verify(x => x.Execute(), Times.Never);
+    }
+    public void Dispose()
+    {
+        Ioc.Resolve<ICommand>("IoC.Scope.Current.Clear").Execute();
+    }
+}
