677590ecfc2ec9c8015775540fd423d07834d6de
Gleb Bindjuk <glebbindjuk@gmail.com>
2024-12-24 19:14:02 +0300

    Formating the files

--- SpaceBattle.Lib/StrategyforMacroCommand.cs
+++ SpaceBattle.Lib/StrategyforMacroCommand.cs
@@ -1,9 +1,9 @@
-namespace SpaceBattle.Lib;
+﻿namespace SpaceBattle.Lib;
 using App;
 
 public class CreateMacroCommandStrategy(string commandSpec)
 {
-    public ICommand Resolve(params object [] args)
+    public ICommand Resolve(params object[] args)
     {
         var names_of_cmds = Ioc.Resolve<string[]>($"Specs.{commandSpec}");
         var cmds = names_of_cmds.Select((x) => Ioc.Resolve<SpaceBattle.Lib.ICommand>((string)x, args[0])).ToArray();
