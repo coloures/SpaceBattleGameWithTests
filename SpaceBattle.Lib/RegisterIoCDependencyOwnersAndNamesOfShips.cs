b9a25a347eee3ed9b0cca4705b78be60b75fe96b
Gleb Bindjuk <glebbindjuk@gmail.com>
2025-03-18 22:27:50 +0600

    Registrations

---
+++ SpaceBattle.Lib/RegisterIoCDependencyOwnersAndNamesOfShips.cs
@@ -0,0 +1,45 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+
+public class RegisterIocDependencyOwnersAndNamesOfShips : ICommand
+{
+    public void Execute()
+    {
+        var OwnersAndNames_of_Ships = new Dictionary<string, List<string>>();
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "OwnersAndNamesOfShipsRepositoryAddShip",
+            (object[] args) =>
+            {
+                return new AddMultipleElemsRepositoryCommand<string>(OwnersAndNames_of_Ships, (string)args[0], (string)args[1]);
+            }
+        ).Execute();
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "OwnersAndNamesOfShipsRepositoryRemoveShip",
+            (object[] args) =>
+            {
+                return new DeleteElemRepositoryCommand<List<string>>(OwnersAndNames_of_Ships, (string)args[0]);
+            }
+        ).Execute();
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "OwnersAndNamesOfShipsRepositoryGetShips",
+            (object[] args) =>
+            {
+                if (OwnersAndNames_of_Ships.TryGetValue((string)args[0], out var result))
+                {
+                    return result;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+        ).Execute();
+
+    }
+}
