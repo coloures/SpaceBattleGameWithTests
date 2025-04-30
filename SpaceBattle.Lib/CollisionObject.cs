4e7e80a9fee053c7b6a18eecc6a0a3d642168db2
9vanilla9 <egor.chamshin@mail.ru>
2025-04-30 08:47:32 +0600

    ColCommand&Test

---
+++ SpaceBattle.Lib/CollisionObject.cs
@@ -0,0 +1,16 @@
+namespace SpaceBattle.Lib;
+
+public class CollisionObject: ICollisionObject
+{
+    public readonly IDictionary<string, object> Object;
+    public readonly IGetterGrid getterGrid;
+    public CollisionObject(IDictionary<string, object> Object, IGetterGrid getterGrid)
+    {
+        this.Object = Object;
+        this.getterGrid = getterGrid;
+    }
+    public IDictionary<string, object> ObjectItself => Object;
+    public int[] GridLocation => getterGrid.GetGridLocation((Vector)Object["Location"]).GetCoordinates();
+    public int[] AbsoluteLocation => ((Vector)Object["Location"]).GetCoordinates();
+    public int[] Velocity => ((Vector)Object["Velocity"]).GetCoordinates();
+}
---
+++ SpaceBattle.Lib/TreeChecker.cs
@@ -0,0 +1,10 @@
+namespace SpaceBattle.Lib;
+using App;
+
+public class TreeChecker: ITreeChecker
+{
+    public void Check(int x, int y, int vel_x, int vel_y)
+    {
+        Ioc.Resolve<ICommand>("shipandtorpedoTreeCheckSet", x, y, vel_x, vel_y).Execute();
+    }
+}
