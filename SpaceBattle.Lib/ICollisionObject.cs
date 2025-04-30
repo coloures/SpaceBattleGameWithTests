a28cd8aac4196be2f225f734fcf4b2856d939ae6
9vanilla9 <egor.chamshin@mail.ru>
2025-04-30 08:43:36 +0600

    ColInt

---
+++ SpaceBattle.Lib/ICollisionObject.cs
@@ -0,0 +1,9 @@
+namespace SpaceBattle.Lib;
+
+public interface ICollisionObject
+{
+    IDictionary<string, object> ObjectItself{get;}
+    int[] GridLocation {get;}
+    int[] AbsoluteLocation {get;}
+    int[] Velocity {get;}
+}
