d8aa4c219772fa301036a36c11f7043ce97bc4c5
Litau-Vladislav <litau05@mail.ru>
2025-04-30 12:53:47 +0600

    Interfaces

---
+++ SpaceBattle.Lib/IGridAdderElem.cs
@@ -0,0 +1,6 @@
+namespace SpaceBattle.Lib;
+
+public interface IGridAdderElem
+{
+    void Add(int x, int y, IDictionary<string, object> ObjectItself, string name_grid);
+}
---
+++ SpaceBattle.Lib/IGridObject.cs
@@ -0,0 +1,6 @@
+namespace SpaceBattle.Lib;
+public interface IGridObject
+{
+    Vector AbsoluteLocation {get;}
+    Vector GridLocation {set;}
+}
