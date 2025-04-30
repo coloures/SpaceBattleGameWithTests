aadec9159565d409c6ca8005b996238e45003319
9vanilla9 <egor.chamshin@mail.ru>
2025-04-30 08:47:32 +0600

    deletefiles

--- SpaceBattle.Lib/CheckCollisionBetweenTwoElemsCommand.cs
+++ SpaceBattle.Lib/CheckCollisionBetweenTwoElemsCommand.cs
@@ -1,4 +1,4 @@
-﻿namespace SpaceBattle.Lib;
+namespace SpaceBattle.Lib;
 
 public class CheckCollisionBetweenTwoElemsCommand : ICommand
 {
--- SpaceBattle.Lib/TreeChecker.cs
+++ SpaceBattle.Lib/TreeChecker.cs
@@ -1,10 +1,21 @@
+<<<<<<< HEAD
 ﻿namespace SpaceBattle.Lib;
 using App;
 
 public class TreeChecker : ITreeChecker
+=======
+namespace SpaceBattle.Lib;
+using App;
+
+public class TreeChecker: ITreeChecker
+>>>>>>> 8a5f295 (ColCommand&Test)
 {
     public void Check(int x, int y, int vel_x, int vel_y)
     {
         Ioc.Resolve<ICommand>("shipandtorpedoTreeCheckSet", x, y, vel_x, vel_y).Execute();
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 8a5f295 (ColCommand&Test)
